61f259d91b6d01788256860c7dd56c367165bd43
Emmanuel PEREIRA <epereira777@gmail.com>
2022-09-14 12:17:24 +0200

    add button for a new quote

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ajax-simpsons-quotes
+
+Ajax request
+------------
+v1
+Put a simpson quote, this name and picture in a DOM using Ajax on API
+
+V2
+Add button for a new quote without relaod
--- main.js
+++ main.js
@@ -9,10 +9,27 @@ function fetchSimpsonJSON() {
       })
       .then(function(simpson) {
         const simpsonHtml = `
-            <p><strong>${simpson[0].quote}</strong> - ${simpson[0].character}</p>
-            <img src="${simpson[0].image}"></img>
+        
+            <blockquote><strong>${simpson[0].quote}</strong> - <em>${simpson[0].character}</em></blockquote></p>
+        
+            <figure class="image is-128x128">
+                <img src="${simpson[0].image}">
+            </figure>
+           
         `;
         document.querySelector('div#chuck-norris').innerHTML = simpsonHtml;
       });
-  }
+}
+
+fetchSimpsonJSON();
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", async(e)=> {
+    e.preventDefault();
+    button.setAttribute("disabled", true);
+    await fetchSimpsonJSON();
+    button.removeAttribute("disabled");
+    return false;
+});
  
